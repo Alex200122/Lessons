7e9a81abfa8d023e998b6b68787714df7f952dc3
Alexander Ivanchenko <fissryba@gmail.com>
2024-06-28 17:27:29 +0300

    h

---
+++ src/ProgramOne.js
@@ -0,0 +1,14 @@
+function fizzBuzz(number) {
+    if (number % 3 === 0 && number % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (number % 3 === 0) {
+        console.log("Fizz");
+    } else if (number % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(number);
+    }
+}
+
+let number = parseInt(prompt("number"));
+fizzBuzz(number);
