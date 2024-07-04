9407892e8f539d0fc8449accfde5f777cfe739e7
Alexander Ivanchenko <fissryba@gmail.com>
2024-07-05 00:31:38 +0300

    HW5

--- HW/HomeWorkFive/hwfivePtwo.js
+++ HW/HomeWorkFive/hwfivePtwo.js
@@ -1,7 +1,16 @@
 function isPalindrome(str) {
+
   let strArray = str.split('')
 
+  let reversedArray = strArray.reverse('')
 
+  let reversedStr = reversedArray.join('')
 
+  if (InputString === reversedStr) return true;
 
+  else { return false;}
 }
+
+let InputString = prompt('')
+let checked = isPalindrome(InputString);
+console.log(`${checked}`);
