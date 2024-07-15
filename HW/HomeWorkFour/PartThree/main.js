964ea60ebd3d14fa6371b7fd751cd07faf5a3480
Alexander Ivanchenko <fissryba@gmail.com>
2024-07-15 15:24:00 +0300

    new commit

--- HW/HomeWorkFour/PartThree/main.js
+++ HW/HomeWorkFour/PartThree/main.js
@@ -1,4 +1,26 @@
-   *
-  ***
- *****
-*******
+function drawTree() {
+   let tree = '';
+
+  for (let i = 0; i < height; i++) 
+   
+   {
+   
+     for (let j = 0; j < height - i - 1; j++) { 
+      tree += ' ';
+     }
+     
+     for (let k = 0; k < 2 * i + 1; k++) { 
+      tree += '*';
+     }
+  
+     if (i < height - 1) { 
+      tree += '\n';
+     }
+
+   }
+
+   return tree;
+
+}
+let height = parseInt(prompt("Висота ялинки"));
+console.log(drawTree());
--- HW/HomeWorkFour/PartTwo/main.js
+++ HW/HomeWorkFour/PartTwo/main.js
@@ -1,17 +1,23 @@
-function Search() {
-
-  let input = prompt("Please enter a number:");
-
-  let N = parseFloat(input);
-
-
-
-  if (number >= N) return false;
-  if (number % N == 0) return console.log("Число N не є досконалим");
-
-  if (N === 2) return console.log("Число N є досконалим");
+function isPerfectNumber(n) {
   
-  else {return ("Число N не є простим числом");}
+  if (n <= 1) {
+      return false;
+  }
 
+  let sum = 0;
+
+  for (let i = 1; i < n; i++) {
+      if (n % i === 0) {
+          sum += i;
+      }
+  }
+
+  return sum === n;
 }
-window.onload = Search;
+
+let number = parseInt(prompt("Ввести число:"));
+if (isPerfectNumber(number)) {
+  console.log(`${number} є досконалим числом.`);
+} else {
+  console.log(`${number} не є досконалим числом.`);
+}
